d10a73beae796d221440ab9904362d4c7718c601
Alan McLarney <alanmclarney@gmail.com>
2017-07-04 15:56:33 +0100

    Complete items before extension work

--- bus.rb
+++ bus.rb
@@ -15,4 +15,17 @@ class Bus
   def count_passenger
     @passengers.count 
   end
+
+  def add_passenger(passenger)
+    @passengers << passenger 
+  end
+
+  def remove_passenger
+    @passengers.shift
+  end
+
+  def empty_bus
+    @passengers.clear
+  end 
+
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -7,6 +7,11 @@ class TestBus < Minitest::Test
 
   def setup
     @passegers = []
+    @passenger1 = Person.new("Dave", 100)
+    @passenger2 = Person.new("Sally", 25)  
+    @passenger3 = Person.new("Bob", 15)
+    @passenger4 = Person.new("Mary", 46)
+
     @bus = Bus.new(22, "Ocean Terminal")
   end 
 
@@ -22,9 +27,20 @@ class TestBus < Minitest::Test
     assert_equal(0, @bus.count_passenger)
   end 
 
-  # def test_add_passenger
-    
-  # end
+  def test_add_passenger
+    @bus.add_passenger(@passenger1)
+    assert_equal(@passenger1, @bus.passengers[0])
+  end
 
+  def test_remove_passenger
+    @bus.add_passenger(@passenger1)
+    @bus.remove_passenger
+    assert_equal([], @bus.passengers)
+  end
+
+  def test_empty_bus
+    @bus.empty_bus
+    assert_equal(0, @bus.count_passenger)
+  end
 
 end
