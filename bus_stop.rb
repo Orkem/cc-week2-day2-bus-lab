99a0887916a888afd6c88b754b73f3ae7b8b9d8a
Chris Hunter <orkem2@gmail.com>
2017-07-04 21:10:23 +0100

    created add_person_to_queue to BusStop class and passed test

--- bus_stop.rb
+++ bus_stop.rb
@@ -1,9 +1,11 @@
 class BusStop
-
+  attr_reader :name, :queue
   def initialize(name)
     @name = name
     @queue = []
-
   end
 
+  def add_person_to_queue(person)
+    @queue << person
+  end
 end 
--- specs/bus_stop_spec.rb
+++ specs/bus_stop_spec.rb
@@ -5,5 +5,18 @@ require_relative('../person.rb')
 
 class TestBusStop < Minitest::Test
 
+  def setup
+   @passenger1 = Person.new("Dave", 100)
+   @passenger2 = Person.new("Sally", 25)  
+   @passenger3 = Person.new("Bob", 15)
+   @passenger4 = Person.new("Mary", 46)
 
+   @bus = Bus.new(22, "Ocean Terminal")
+   @bus_stop1 = BusStop.new("Leith")
+  end
+
+  def test_add_person_to_queue()
+    @bus_stop1.add_person_to_queue(@passenger1)
+    assert_equal(@passenger1, @bus_stop1.queue[0])
+  end
 end
